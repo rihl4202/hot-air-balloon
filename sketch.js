4adac5889bb972d05a7a314ad23fd25069e93199
rihl4202 <88212189+rihl4202@users.noreply.github.com>
2021-10-15 17:54:19 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,39 @@
+var ball;
+
+function setup(){
+    createCanvas(500,500);
+    ball = createSprite(250,250,10,10);
+    ball.shapeColor = "red";
+    database = firebase.database()
+    //reads the values from the database
+    database.ref("ball/position").on("value",function(data){
+      var pos = data.val()
+      ball.x = pos.x
+      ball.y = pos.y
+    })
+}
+
+function draw(){
+    background("white");
+    if(keyDown(LEFT_ARROW)){
+        changePosition(-1,0);
+    }
+    else if(keyDown(RIGHT_ARROW)){
+        changePosition(1,0);
+    }
+    else if(keyDown(UP_ARROW)){
+        changePosition(0,-1);
+    }
+    else if(keyDown(DOWN_ARROW)){
+        changePosition(0,+1);
+    }
+    drawSprites();
+}
+//writing the values to the database
+function changePosition(x,y){
+   
+  database.ref("ball/position").set({
+    x:ball.x+x,
+    y:ball.y+y
+  })
+}
